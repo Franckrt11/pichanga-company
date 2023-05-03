5bc5b50f779a02140ff1e30ccf8d4ae94e3f65e7
Lauc1an <marlomp@hotmail.com>
2023-05-03 05:14:18 -0500

    Creating Auth Context

---
+++ models/auth.js
@@ -0,0 +1,40 @@
+import { API_URL } from "@env";
+import * as Device from "expo-device";
+
+const fetchHeaders = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+
+const handleLogin = async (email, password) => {
+  const response = await fetch(`${API_URL}api/company/login`, {
+    method: "POST",
+    headers: fetchHeaders,
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      // device: Device.modelId,
+      device: 'MiIphone12', // Device temporal
+    }),
+  });
+
+  return await response.json();
+};
+
+const handleRegister = async (email, password) => {
+  const response = await fetch(`${API_URL}api/company/register`, {
+    method: "POST",
+    headers: fetchHeaders,
+    body: JSON.stringify({
+      email: email,
+      password: password,
+      status: true,
+      // device: Device.modelId,
+      device: 'MiIphone12', // Device temporal
+    }),
+  });
+
+  return await response.json();
+};
+
+export { handleLogin, handleRegister };

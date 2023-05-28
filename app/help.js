aca47ab8b48bd310e251d89e8b17c8516cb5a30a
Lauc1an <marlomp@hotmail.com>
2023-05-28 00:08:53 -0500

    Fix help page

--- app/help.js
+++ app/help.js
@@ -1,6 +1,6 @@
-import { Text, KeyboardAvoidingView } from "react-native";
+import { Text, KeyboardAvoidingView, View, StyleSheet } from "react-native";
 import { Stack } from "expo-router";
-import { LayoutStyles } from "../src/constants/styles";
+import { LayoutStyles, Colors } from "../src/constants/styles";
 import Back from "../src/components/header/back";
 
 const Help = () => {
@@ -18,9 +18,31 @@ const Help = () => {
           headerLeft: () => <Back />,
         }}
       />
-      <Text>Help</Text>
+      <View style={{ width: "90%" }}>
+        <Text style={styles.title}>AYUDA</Text>
+        <Text style={styles.content}>Puedes comunicarte al</Text>
+        <Text style={[styles.content, { fontSize: 17, fontFamily: "PoppinsSemiBold",}]}>soporte@tejuegounapichanga.com</Text>
+        <Text style={[styles.content, { fontSize: 17, fontFamily: "PoppinsSemiBold",}]}>(+51) 982 984 877</Text>
+      </View>
     </KeyboardAvoidingView>
   );
 };
 
 export default Help;
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    color: Colors.maastrichtBlue,
+    fontSize: 28,
+    fontFamily: "PoppinsSemiBold",
+    marginBottom: 10,
+  },
+  content: {
+    color: Colors.maastrichtBlue,
+    fontSize: 15,
+    fontFamily: "PoppinsMedium",
+    marginBottom: 10,
+    textAlign: "left"
+  },
+});

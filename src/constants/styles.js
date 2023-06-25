2d8128853dafdaa588572afc2a830c862937e5dc
Lauc1an <marlomp@hotmail.com>
2023-06-25 16:30:37 -0500

    Creating desing of Home Page

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -5,7 +5,8 @@ export const Colors = {
   maastrichtBlue: '#00182E',
   metallicGreen: '#357A03',
   silverSand: '#B7C0C5',
-  greenLizard: '#A4E624'
+  greenLizard: '#A4E624',
+  ferrariRed: '#e72c01'
 };
 
 export const AppImages = {

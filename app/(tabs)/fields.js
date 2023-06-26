8139f3c90a4666bbc6ca5ba14f03b85987e93f8f
Lauc1an <marlomp@hotmail.com>
2023-06-26 00:59:45 -0500

    Creating desing of Field Page

--- app/(tabs)/fields.js
+++ app/(tabs)/fields.js
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
 import React from "react";
 import { LayoutStyles } from '../../src/constants/styles';
+import FieldItem from "../../src/components/field-item";
 
 const Fields = () => {
   return (
-    <View style={ LayoutStyles.whiteContainer }>
-      <Text>Fields</Text>
-    </View>
+    <SafeAreaView
+      style={[LayoutStyles.whiteContainer, { justifyContent: "flex-start" }]}
+    >
+      <ScrollView style={{ marginTop: 10, width: "90%" }}>
+        <FieldItem name={"Cancha 1"} active={true} />
+        <FieldItem name={"Cancha 2"} active={false} />
+        <FieldItem name={"Cancha 3"} active={true} />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
---
+++ src/components/field-item.js
@@ -0,0 +1,162 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Switch,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Fontisto from "react-native-vector-icons/Fontisto";
+import { Colors } from "../../src/constants/styles";
+import StarIcon from "../components/icons/star-icon";
+import PencilIcon from "../components/icons/pencil-icon";
+
+const FieldItem = ({ name, active }) => {
+  const [visible, setVisible] = useState(active);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => console.log("Change order")}>
+        <Ionicons name="menu" size={32} color={Colors.silverSand} />
+      </TouchableOpacity>
+      <View style={styles.wrapper}>
+        <View style={styles.header}>
+          <Image
+            source={require("../assets/cancha-lorem.jpg")}
+            style={styles.image}
+          />
+          <View style={styles.content}>
+            <View style={styles.description}>
+              <Text style={styles.title}>{name}</Text>
+              <Text style={styles.subtitle}>San Juan de Miraflores</Text>
+              <View style={styles.score}>
+                <StarIcon size={24} active={true} />
+                <StarIcon size={24} active={true} />
+                <StarIcon size={24} active={true} />
+                <StarIcon size={24} active={false} />
+                <StarIcon size={24} active={false} />
+                <Text style={styles.countScore}>(50)</Text>
+              </View>
+            </View>
+            <View style={styles.more}>
+              <TouchableOpacity onPress={() => console.log("View more detail")}>
+                <Fontisto
+                  name="zoom-plus"
+                  size={24}
+                  color={Colors.maastrichtBlue}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={styles.footer}>
+          <TouchableOpacity
+            onPress={() => console.log("View comments")}
+            style={styles.buttom}
+          >
+            <Text style={styles.buttomText}>Comentarios</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => console.log("Edit field")}
+            style={styles.buttom}
+          >
+            <PencilIcon />
+            <Text style={styles.buttomText}>Editar fotos</Text>
+          </TouchableOpacity>
+          <Switch
+            trackColor={{ false: Colors.silverSand, true: Colors.silverSand }}
+            thumbColor={visible ? Colors.greenLizard : Colors.maastrichtBlue}
+            ios_backgroundColor={Colors.maastrichtBlue}
+            onValueChange={() => console.log("Switch visible field")}
+            value={visible}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default FieldItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+    marginBottom: 25,
+  },
+  wrapper: {
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: Colors.silverSand,
+    overflow: "hidden",
+    flex: 1,
+  },
+  header: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.silverSand,
+  },
+  image: {
+    height: 150,
+    width: 100,
+  },
+  content: {
+    flexDirection: "row",
+    paddingVertical: 12,
+    flex: 1,
+    borderLeftWidth: 1,
+    borderLeftColor: Colors.silverSand,
+  },
+  description: {
+    borderRightWidth: 2,
+    borderRightColor: Colors.silverSand,
+    flex: 1,
+    paddingVertical: 5,
+    paddingLeft: 15,
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: "PoppinsMedium",
+  },
+  subtitle: {
+    fontSize: 18,
+    fontFamily: "PoppinsMedium",
+  },
+  score: {
+    flexDirection: "row",
+    gap: 5,
+    marginTop: 8,
+  },
+  countScore: {
+    fontSize: 18,
+    fontFamily: "PoppinsMedium",
+  },
+  more: {
+    justifyContent: "center",
+    paddingHorizontal: 12,
+  },
+  footer: {
+    backgroundColor: Colors.maastrichtBlue,
+    flexDirection: "row",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  buttom: {
+    backgroundColor: Colors.white,
+    borderRadius: 15,
+    paddingVertical: 3,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+  buttomText: {
+    color: Colors.maastrichtBlue,
+    fontFamily: "PoppinsMedium",
+  },
+});
